9fdb176de18e8b83f298a32941837811ddfd242c
Robbe Bryssinck <robbebryssinck@gmail.com>
2019-11-13 11:46:29 -0800

    The server now accepts input and saves the IP-address and the message to the logs.txt file

--- chatServer/server.py
+++ chatServer/server.py
@@ -1,22 +1,67 @@
 import socket
 import sys
+import os
+import optparse
+from threading import *
 
-# Create a TCP socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+screenLock = Semaphore(value=5)
+global logs
 
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print("starting up on {} port {}".format(*server_address))
-s.bind(server_address)
+def createServer(port):
 
-while True:
-    print("\nWaiting to receive message")
-    data, address = s.recvfrom(4096)
+	# create a TCP socket
+	sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    print("Received {} bytes from {}".format(len(data), address))
-    print(data)
+	# bind the socket to the port
+	server_address = ('localhost', port)
+	print("starting up on {} port {}".format(*server_address))
+	sck.bind(server_address)
 
-    if data:
-        sent = sock.sendto(data, address)
-        print("Sent {} bytes back to {}".format(sent, address))
+	# put the socket into server mode
+	sck.listen(5)
 
+	return sck
+
+
+def runServer(sck, conn, client, logs):
+
+	
+	data = conn.recv(1024)
+	message = client[0] + ': ' + data.decode() + '\n'
+	print(message)
+	logs.write(message)
+
+	conn.close()
+
+
+def main():
+
+	# option to set port when launching the server
+	parser = optparse.OptionParser("Usage: pyhon3 server.py -p <server port>")
+	parser.add_option('-p', dest='port', type='int', help="specify target port")
+	(options, args) = parser.parse_args()
+	port = options.port
+	
+	if port == None:
+		print(parser.usage)
+		exit(0)
+
+	# create server logs
+	if not os.path.exists('./logs.txt'):
+		os.mknod('./logs.txt')
+	logs = open('./logs.txt', 'w')
+
+	# create the socket
+	sck = createServer(port)
+
+	# run the server
+	while True:
+
+		# wait for connection and start thread
+		conn, client = sck.accept()
+		t = Thread(target=runServer, args=(sck, conn, client, logs))
+		t.start()
+
+
+if __name__ == '__main__':
+	main()
