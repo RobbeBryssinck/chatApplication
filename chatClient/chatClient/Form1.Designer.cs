97f0418b278d3e47022b4732a122b8e0c578cda1
Robbe Bryssinck <robbebryssinck@gmail.com>
2019-11-16 14:10:41 +0100

    Added client GUI

--- chatClient/chatClient/Form1.Designer.cs
+++ chatClient/chatClient/Form1.Designer.cs
@@ -28,33 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.libChat = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.btDisconnect = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // libChat
+            // 
+            this.libChat.FormattingEnabled = true;
+            this.libChat.ItemHeight = 16;
+            this.libChat.Location = new System.Drawing.Point(6, 41);
+            this.libChat.Name = "libChat";
+            this.libChat.Size = new System.Drawing.Size(525, 292);
+            this.libChat.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chat history:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Send message:";
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Controls.Add(this.btSend);
+            this.groupBox1.Controls.Add(this.tbMessage);
+            this.groupBox1.Controls.Add(this.libChat);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(245, 11);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(776, 376);
-            this.groupBox1.TabIndex = 0;
+            this.groupBox1.Size = new System.Drawing.Size(543, 452);
+            this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Chat view";
+            this.groupBox1.Text = "Chat";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(6, 365);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(525, 22);
+            this.tbMessage.TabIndex = 3;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(191, 404);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(162, 42);
+            this.btSend.TabIndex = 4;
+            this.btSend.Text = "Send message";
+            this.btSend.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btDisconnect);
+            this.groupBox2.Controls.Add(this.btConnect);
+            this.groupBox2.Controls.Add(this.tbIP);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.tbPort);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tbName);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 11);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(218, 452);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Connection";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Name:";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(6, 41);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(206, 22);
+            this.tbName.TabIndex = 6;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(6, 157);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(206, 22);
+            this.tbPort.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Port:";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(6, 97);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(206, 22);
+            this.tbIP.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "IP-address:";
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(6, 201);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(206, 63);
+            this.btConnect.TabIndex = 5;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            // 
+            // btDisconnect
+            // 
+            this.btDisconnect.Location = new System.Drawing.Point(6, 299);
+            this.btDisconnect.Name = "btDisconnect";
+            this.btDisconnect.Size = new System.Drawing.Size(206, 63);
+            this.btDisconnect.TabIndex = 11;
+            this.btDisconnect.Text = "Disconnect";
+            this.btDisconnect.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 475);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Chat application";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.ListBox libChat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btDisconnect;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label3;
     }
 }
 
