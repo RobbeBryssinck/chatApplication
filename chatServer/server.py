1eb1fca795cefbf682b090a49304a7972aac0781
Robbe Bryssinck <robbebryssinck@gmail.com>
2019-11-18 04:16:53 -0800

    Implement broadcasting server side

--- chatServer/server.py
+++ chatServer/server.py
@@ -27,16 +27,10 @@ def clientHandler(sck, conn, client, logs):
 	while True:
 		try:
 			data = conn.recv(1024)
-			if data != b'/dc':
-				message = client[0] + ': ' + data.decode() + '\n'
-				print(message)
-				logs.write(message)
-			else:
-				message = client[0] + " closed the connection.\n"
-				logs.write(message)
-				print(message)
-				break
-
+			message = client[0] + ': ' + data.decode() + '\n'
+			print(message)
+			updateClients(message, conn)
+			logs.write(message)
 		except:
 			message = client[0] + " closed the connection.\n"
 			logs.write(message)
@@ -46,10 +40,15 @@ def clientHandler(sck, conn, client, logs):
 	conn.close()
 
 
+def updateClients(message, conn):
+	for client in clients:
+		client.send(bytes(message, 'ASCII'))
+
+
 def main():
 
 	# option to set port when launching the server
-	parser = optparse.OptionParser("Usage: pyhon3 server.py -p <server port>")
+	parser = optparse.OptionParser("Usage: python3 server.py -p <server port>")
 	parser.add_option('-p', dest='port', type='int', help="specify target port")
 	(options, args) = parser.parse_args()
 	port = options.port
@@ -58,16 +57,16 @@ def main():
 		print(parser.usage)
 		exit(0)
 
-	# create server logs
 	logs = open('./logs.txt', 'a+')
-
-	# create the socket
 	sck = createServer(port)
+	
 
 	while True:
 
 		# wait for connection
 		conn, client = sck.accept()
+
+		clients.append(conn)
 
 		# log connection
 		message = client[0] + " connected.\n"
@@ -78,6 +77,7 @@ def main():
 		t = Thread(target=clientHandler, args=(sck, conn, client, logs))
 		t.start()
 
+clients = []
 
 if __name__ == '__main__':
 	main()
